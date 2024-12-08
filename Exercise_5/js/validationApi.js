911c8961ebd5c66ad40a9338a1804bc75dd2110a
serkosam <serkosam@gmail.com>
2024-12-08 13:07:00 +0200

    education check func complete

--- Exercise_5/js/validationApi.js
+++ Exercise_5/js/validationApi.js
@@ -1,46 +1,54 @@
 window.addEventListener('DOMContentLoaded', main)
 
 function main() {
-    console.log("aaaaaaaa");
-    // Get the form and error message elements
     const form = document.getElementById('form');
-    const password1 = document.getElementById('password1');
-    const password2 = document.getElementById('password2');
-    const languagesCheckboxes = document.querySelectorAll('input[name="languages"]'); // Get all language checkboxes
 
-    // Add event listener for form submission
     form.addEventListener('submit', function(event) {
-        checkCorrectPassword(password1, password2, event)
-        checkLanguages(languagesCheckboxes, event)
+        checkCorrectPassword(event)
+        checkLanguages(event)
+        checkEducationLevelHighOrOtherThenTopicOther(event)
     });
 }
 
-function checkCorrectPassword(password1, password2, event) {
-    // Prevent form submission if passwords do not match
+function checkCorrectPassword(event) {
+    const password1 = document.getElementById('password1');
+    const password2 = document.getElementById('password2');
     const errorMessage = document.getElementById('pass-error');
     if (password1.value !== password2.value) {
-        errorMessage.style.display = 'block';  // Show error message
-        password2.style.border = '2.5px solid red';  // Make border red
-        event.preventDefault();  // Prevent form from being submitted
+        errorMessage.style.display = 'block';
+        password2.style.border = '2.5px solid red';
+        event.preventDefault();
     } else {
         password2.style.border = '';
-        errorMessage.style.display = 'none';  // Hide error message
+        errorMessage.style.display = 'none';
     }
 }
 
-function checkLanguages(languagesCheckboxes, event) {
-    // Check if at least one language checkbox is selected
+function checkLanguages(event) {
+    const languagesCheckboxes = document.querySelectorAll('input[name="languages"]');
     const errorMessage = document.getElementById('lang-error');
     const isChecked = Array.from(languagesCheckboxes).some(checkbox => checkbox.checked);
 
     if (!isChecked) {
-        errorMessage.style.display = 'block';  // Show error message if no language is selected
+        errorMessage.style.display = 'block';
         event.preventDefault();
     } else {
-        errorMessage.style.display = 'none';  // Hide error message if at least one language is selected
+        errorMessage.style.display = 'none';
     }
 }
 
-function checkEducationLevelHighOrOtherThenTopicOther() {
+function checkEducationLevelHighOrOtherThenTopicOther(event) {
+    const educationlevel = document.getElementById("educationLevel");
+    const educationtopic = document.getElementById("educationTopic");
+    const errorMessage = document.getElementById('edu-error');
 
+    const valueLevel = educationlevel.options[educationlevel.selectedIndex].value;
+    const valueTopic = educationtopic.options[educationtopic.selectedIndex].value;
+
+    if ((valueLevel == "highschool" || valueLevel == "other") && valueTopic !== "other" ){
+        errorMessage.style.display = 'block';
+        event.preventDefault();
+    }else {
+        errorMessage.style.display = 'none';
+    }
 }
